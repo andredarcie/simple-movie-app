af3230c8a980d053fe1627659c4bfcd502fa2719
André N. Darcie <andrendarcie@gmail.com>
2016-12-20 15:47:44 -0200

    Added search for movies by name

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -11,9 +11,16 @@ namespace MvcMovie.Controllers
         private MovieDBContext db = new MovieDBContext();
 
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
-            return View(db.Movies.ToList());
+            var movies = from m in db.Movies select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(movies);
         }
 
         // GET: Movies/Details/5
